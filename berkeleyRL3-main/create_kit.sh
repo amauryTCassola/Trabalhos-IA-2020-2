c2e348c0963c91ffb23d96f6b1512eedcbbe04f0
guiRodrigues <guilherme.rodrigues@inf.ufrgs.br>
2021-05-03 20:50:15 -0300

    new Grader

--- berkeleyRL3-main/create_kit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-zip -r reinforcement.zip reinforcement --exclude *.idea* --exclude *.git* --exclude *.pyc -q
-echo "If there were no errors, the kit has been created in reinforcement.zip"
-
